d9f120e24b3a2a043ddb80f110725d3ced349ebc
Ignacio Delgado <ignacio.delgado@ding.com>
2016-01-16 11:32:46 +0000

    Initial Commit

---
+++ SoundTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoundTest
+//
+//  Created by Ignacio Delgado on 16/01/2016.
+//  Copyright © 2016 ID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoundTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SoundTest
+//
+//  Created by Ignacio Delgado on 16/01/2016.
+//  Copyright © 2016 ID. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
